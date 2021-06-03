c8cc40489acdb32030956cb5ff0d8bb92b5c4745
thanhdoan <thanh.doan1@rbi.co.uk>
2021-06-03 10:52:08 +0100

    Init

--- FPC/Excercises/Chapter01/Examples.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-
-namespace Excercises.Chapter01
-{
-    public static class Examples
-    {
-        public static Func<T2, T1, R> SwapArgs<T1, T2, R>(this Func<T1, T2, R> func) => (t1, t2) => func(t2, t1);
-    }
-}
